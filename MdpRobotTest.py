7a7256f4131ac0887b254fb29440f036d0ceeba5
Chris Baek <chris@Chriss-MacBook-Pro-3.local>
2018-10-17 23:38:46 -0700

    fixed eval_trajectory functions and cleaned up the test script

--- MdpRobotTest.py
+++ MdpRobotTest.py
@@ -11,59 +11,63 @@ robot = MdpRobot.MdpRobot(6,6)
 initial_policy = policy.Policy()
 initial_state = state.State(1,4,6)
 
-#robot.plot_trajectory(initial_policy, initial_state, 0)
+trajectory = robot.plot_trajectory(initial_policy, initial_state, 0)
+print trajectory
 
-#prob 3(d)
+#prob 3(e)
 discount_factor = 0.9
-#value = robot.eval_policy(initial_policy, discount_factor)
-
-# print(value)
-
-# #prob 3(e)
-# print(value[6][4][1])
-
-# print value
-
-# # prob 3(e)
-# x, y, heading = initial_state.get_state()
-# print value[heading][x][y]
+# value = robot.eval_policy(initial_policy, discount_factor)
+# pickle.dump(value, open("value.p", "wb"))
+value = pickle.load(open("value.p", "rb"))
+x, y, heading = initial_state.get_state()
+print "3(d): ", value[heading][x][y] # the value matrix is indexed by heading, x, and y.
 
 # prob 3(h) and (i)
-#start_time = time.time()
-#opt_policy_prob3, opt_value_prob3 = robot.policy_iteration(initial_policy, discount_factor)
+start_time = time.time()
+opt_policy_prob3, opt_value_prob3 = robot.policy_iteration(initial_policy, discount_factor)
 # pickle.dump(opt_policy_prob3, open("opt_policy_prob3.p", "wb"))
 # pickle.dump(opt_value_prob3, open("opt_value_prob3.p", "wb"))
 # opt_policy_prob3 = pickle.load(open("opt_policy_prob3.p", "rb"))
 # opt_value_prob3 = pickle.load(open("opt_value_prob3.p", "rb"))
-#end_time = time.time()
-# elapsed_time = end_time - start_time
-# print "3(i): ", elapsed_time, "seconds"
-#robot.plot_trajectory(opt_policy_prob3, initial_state, 0)
+end_time = time.time()
+elapsed_time = end_time - start_time
+print "3(i): ", elapsed_time, "seconds"
+trajectory = robot.plot_trajectory(opt_policy_prob3, initial_state, 0)
+print trajectory
+print "Value of initial state = ", opt_value_prob3[heading][x][y]
+
 
 # prob 4(b) and 4(c)
-#start_time = time.time()
-#opt_policy_prob4, opt_value_prob4 = robot.value_iteration(discount_factor)
+start_time = time.time()
+opt_policy_prob4, opt_value_prob4 = robot.value_iteration(discount_factor)
 # pickle.dump(opt_policy_prob4, open("opt_policy_prob4.p", "wb"))
 # pickle.dump(opt_value_prob4, open("opt_value_prob4.p", "wb"))
-#end_time = time.time()
-#elapsed_time = end_time - start_time
-#print "4(c): ", elapsed_time, "seconds"
-#robot.plot_trajectory(opt_policy_prob4, initial_state, 0)
+# opt_policy_prob4 = pickle.load(open("opt_policy_prob4.p", "rb"))
+# opt_value_prob4 = pickle.load(open("opt_value_prob4.p", "rb"))
+end_time = time.time()
+elapsed_time = end_time - start_time
+print "4(c): ", elapsed_time, "seconds"
+trajectory = robot.plot_trajectory(opt_policy_prob4, initial_state, 0)
+print trajectory
+print "Value of initial state = ", opt_value_prob4[heading][x][y]
 
 # prob 5(a)
-#trajectory = robot.plot_trajectory(initial_policy, initial_state, 0.25)
-#value = robot.eval_trajectory(discount_factor, trajectory)
-#print value
-
+trajectory = robot.plot_trajectory(initial_policy, initial_state, 0.25)
+value_prob5a = robot.eval_trajectory(discount_factor, trajectory)
+print trajectory
+print "5(a): Value of trajectory = ", value_prob5a
 
 # prob 5(b)
 error_prob = 0
-trajectory = robot.plot_trajectory_prob5b(initial_policy, initial_state, error_prob)
-value = robot.eval_trajectory_prob5b(discount_factor, trajectory)
-print value
+trajectory = robot.plot_trajectory(initial_policy, initial_state, error_prob)
+value_prob5b_0 = robot.eval_trajectory_prob5b(discount_factor, trajectory)
+print trajectory
+print "5(b): Value of trajectory (0%) = ", value_prob5b_0
 
 error_prob = 0.25
 trajectory = robot.plot_trajectory_prob5b(initial_policy, initial_state, error_prob)
-value = robot.eval_trajectory_prob5b(discount_factor, trajectory)
-print value
-import pdb; pdb.set_trace()
+value_prob5_25 = robot.eval_trajectory_prob5b(discount_factor, trajectory)
+print trajectory
+print "5(b) Value of trajectory (25%) = ", value_prob5_25
+
+# import pdb; pdb.set_trace()
